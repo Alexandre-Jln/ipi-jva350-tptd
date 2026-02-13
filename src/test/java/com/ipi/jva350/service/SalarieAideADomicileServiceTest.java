19f88e6082cd4899a6e400a2e618f1cf55ef55fe
Yann Bernard <yannbernard61@gmail.com>
2026-02-13 15:52:55 +0100

    ajout du test calculentrepriselimitecongepermis

--- src/test/java/com/ipi/jva350/service/SalarieAideADomicileServiceTest.java
+++ src/test/java/com/ipi/jva350/service/SalarieAideADomicileServiceTest.java
@@ -1,6 +1,7 @@
 package com.ipi.jva350.service;
 
 import com.ipi.jva350.exception.SalarieException;
+import com.ipi.jva350.model.Entreprise;
 import com.ipi.jva350.model.SalarieAideADomicile;
 import com.ipi.jva350.repository.SalarieAideADomicileRepository;
 import org.junit.jupiter.api.Test;
@@ -23,5 +24,70 @@ class SalarieAideADomicileServiceTest {
 
     @InjectMocks
     private SalarieAideADomicileService service;
+
+    @Test
+    void calculeLimiteSansMalusBonus() {
+
+        // GIVEN
+        // Le repository est mocké
+        when(salarieAideADomicileRepository.partCongesPrisTotauxAnneeNMoins1())
+                .thenReturn(0.0);
+
+        LocalDate moisEnCours = LocalDate.of(2024, 3, 1);
+        LocalDate debutContrat = LocalDate.of(2024, 3, 1);
+        LocalDate debut = LocalDate.of(2024, 4, 1);
+        LocalDate fin = LocalDate.of(2024, 4, 1);
+
+        double congesN1 = 24;
+
+        // WHEN
+        long limite = service.calculeLimiteEntrepriseCongesPermis(
+                moisEnCours, congesN1, debutContrat, debut, fin
+        );
+
+        // THEN
+        double proportion = Entreprise.proportionPondereeDuMois(debut);
+        long attendu = Math.round(proportion * congesN1);
+
+        assertEquals(attendu, limite);
+    }
+
+
+    @Test
+    void calculeLimiteAvecBonusMalus() {
+
+        // GIVEN
+        // Cette fois on active le malus/bonus
+        when(salarieAideADomicileRepository.partCongesPrisTotauxAnneeNMoins1())
+                .thenReturn(0.5);
+
+        LocalDate moisEnCours = LocalDate.of(2024, 6, 1);
+        LocalDate debutContrat = LocalDate.of(2024, 6, 1);
+        LocalDate debut = LocalDate.of(2024, 6, 1);
+        LocalDate fin = LocalDate.of(2024, 6, 1);
+
+        double congesN1 = 30;
+
+        // WHEN
+        long limite = service.calculeLimiteEntrepriseCongesPermis(
+                moisEnCours, congesN1, debutContrat, debut, fin
+        );
+
+        // THEN
+        double proportion = Entreprise.proportionPondereeDuMois(debut);
+        double base = proportion * congesN1;
+
+        double proportionMoisEnCours =
+                ((6 - Entreprise.getPremierJourAnneeDeConges(moisEnCours).getMonthValue()) % 12) / 12d;
+
+        double diff = proportionMoisEnCours - 0.5;
+
+        // Bonus/malus = 20% de cette différence * congés N-1
+        double bonus = diff * 0.2 * congesN1;
+
+        long attendu = Math.round(base + bonus);
+
+        assertEquals(attendu, limite);
+    }
 
 }
